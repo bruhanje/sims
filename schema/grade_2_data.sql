e6f0d6bff0a5e0fb443d4cd3e28c5c389b7f714f
cyf <1106572144@qq.com>
2020-12-13 20:18:19 +0800

    列出课程

---
+++ schema/grade_2_data.sql
@@ -0,0 +1,24 @@
+﻿DELETE FROM course_grade;
+DELETE FROM course;
+DELETE FROM student;
+
+INSERT INTO student (sn, no, name,gender)  VALUES
+    (101, 'S001',  '张三','男'),
+    (102, 'S002',  '李四','女'), 
+    (103, 'S003',  '王五','男'),
+    (104, 'S004',  '马六','女');
+
+INSERT INTO course (sn, no, name,place,time)  VALUES 
+    (101, 'C01',  '高数','一公教','一学期'), 
+    (102, 'C02',  '外语','二公教','二学期'),
+    (103, 'C03',  '线代','一公教','二学期');
+
+
+INSERT INTO course_grade (stu_sn, cou_sn, grade)  VALUES 
+    (101, 101,  91), 
+    (102, 101,  89),
+    (103, 101,  90),
+    (101, 102,  89);
+
+
+    
